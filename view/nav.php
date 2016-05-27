d9e61ff4eb51e1b7f78328887b41049e10c1b426
elgrim312 <alexandre.vagnair@hetic.net>
2016-05-27 12:47:37 +0200

    hello

--- view/nav.php
+++ view/nav.php
@@ -1,15 +1,15 @@
 <nav class="navbar navbar-inverse navbar-fixed-top">
     <div class="container">
         <div class="navbar-header">
-            <a class="navbar-brand" href="?p=">WtfWeb</a>
+            <a class="navbar-brand" href="?p=teletobise">WtfWeb</a>
         </div>
         <div id="navbar" class="navbar-collapse collapse">
             <ul class="nav navbar-nav">
-                <?php foreach ($nav as $link) : ?>
-                    <li <?= Controller\PageController::Active($_GET['p'], $link->slug)?>>
-                        <a  href="?p=<?= $link->slug ?>"><?= $link->slug ?></a>
+                <?php foreach ($nav as $wtf) { ?>
+                    <li <?= Controller\PageController::Active($_GET['p'], $wtf->slug)?>>
+                        <a  href="?p=<?= $wtf->slug ?>"><?= $wtf->slug ?></a>
                     </li>
-                <?php endforeach ?>
+                <?php } ?>
             </ul>
         </div>
     </div>
